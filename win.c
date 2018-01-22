0675678ba7cf61a23156a59036f474874dc1451b
concerned-guy <34766392+concerned-guy@users.noreply.github.com>
2018-01-22 10:56:53 +0700

    added win.c

---
+++ win.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("How to win without modifying main function!\n");
+	if (1 > 2) {
+		printf("You win!\n");
+		exit(1);
+	}
+	printf("You lose!\n");
+}
